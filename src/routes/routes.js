9dccdfec2d4668ad241108e45e32d0acccc2f320
paulohenrique1993 <paulohsilva.eq@gmail.com>
2021-03-03 21:43:01 -0300

    backend com SelectAll e SelectDetail

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,5 +3,8 @@ const ClienteController = require ('../controllers/clienteController.js');
 const router = express.Router();
  
 router.post('/cliente', ClienteController.Insert);
+router.get('/cliente', ClienteController.SelectAll);
+router.get('/cliente/:id', ClienteController.SelectDetail); 
  
 module.exports = router;
+
