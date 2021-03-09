de2d53b07513b593e08194ffdfa270e16303481c
paulohenrique1993 <paulohsilva.eq@gmail.com>
2021-03-09 20:22:15 -0300

    Backend com Update e Delete

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const ClienteController = require ('../controllers/clienteController.js');
+const ClienteController = require('../controllers/clienteController.js');
 const router = express.Router();
  
 router.post('/cliente', ClienteController.Insert);
 router.get('/cliente', ClienteController.SelectAll);
-router.get('/cliente/:id', ClienteController.SelectDetail); 
+router.get('/cliente/:id', ClienteController.SelectDetail);
+router.put('/cliente/:id', ClienteController.Update);
+router.delete('/cliente/:id', ClienteController.Delete);
  
 module.exports = router;
-
